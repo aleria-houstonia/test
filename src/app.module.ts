c81e9a469b8f5e3ce67b81ce19748559a55b4a95
aiperi <uwuowo733@gmail.com>
2022-07-28 15:25:23 +0600

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers:[AppController],
-    providers:[AppService]
+  controllers:[],
+  providers:[],
+  imports:[
+    SequelizeModule.forRoot({ 
+      dialect: 'postgres', 
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest-project',
+      models: [],
+      autoLoadModels:true 
+    }),
+    UsersModule,]
 })
+
+
+// @Module({
+//     controllers:[AppController],///чтобы контроллеры заработали, мы регистрируем в модули. Создаемм поле контроллерс (массив)
+//     providers:[AppService],//все что содержит логику и может исп-ся в др компонентах
+//     imports:[ ///когда нужно импортировать др молули
+//       SequelizeModule.forRoot({ // секвалайс модуль, в к-ом указ-ся конфиг для бд 
+//         dialect: 'postgres', // бд
+//         host: 'localhost',
+//         port: 5432,
+//         username: 'postgres',
+//         password: 'root',
+//         database: 'nest-project',
+//         models: [],
+//         autoLoadModels:true //чтобы секвалайс создавал таблицы в бд на основании тех моделей,к-ые будем создавать
+//       }),]
+// })
 
 export class AppModule{}
