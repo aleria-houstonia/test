67da4a5924a02c5f9ac8e054d1bf68511b5d75d6
aiperi <uwuowo733@gmail.com>
2022-10-06 00:12:47 +0300

    clearing

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,20 +20,6 @@ import { UsersModule } from './users/users.module';
 })
 
 
-// @Module({
-//     controllers:[AppController],///чтобы контроллеры заработали, мы регистрируем в модули. Создаемм поле контроллерс (массив)
-//     providers:[AppService],//все что содержит логику и может исп-ся в др компонентах
-//     imports:[ ///когда нужно импортировать др молули
-//       SequelizeModule.forRoot({ // секвалайс модуль, в к-ом указ-ся конфиг для бд 
-//         dialect: 'postgres', // бд
-//         host: 'localhost',
-//         port: 5432,
-//         username: 'postgres',
-//         password: 'root',
-//         database: 'nest-project',
-//         models: [],
-//         autoLoadModels:true //чтобы секвалайс создавал таблицы в бд на основании тех моделей,к-ые будем создавать
-//       }),]
-// })
+
 
 export class AppModule{}
--- tutor/app.service.ts
+++
@@ -1,8 +0,0 @@
-// import { Injectable } from "@nestjs/common";
-
-// @Injectable()/// чтобы класс стал провайдером 
-// export class AppService{
-//     getUsers(){
-//         return [{id:1,name:"Aiperi"}]
-//     }
-// }
