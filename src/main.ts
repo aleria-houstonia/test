f8d1a8ee147e898b5097b4a3e0fc116a607417b8
aleria-houstonia <uwuowo733@gmail.com>
2023-01-17 16:06:33 +0300

    dto doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder } from "@nestjs/swagger"
+import { SwaggerModule } from "@nestjs/swagger/dist"
 import { AppModule } from "./app.module"
 
 async function start() {
-    const PORT=process.env.PORT||5000
-    const app = await NestFactory.create(AppModule) ///создание экземпляра  нашего приложения
+    const PORT=process.env.PORT||5000;
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder().setTitle('Test Nestjs')
+                                        .setDescription('Docs Rest Api')
+                                        .setVersion("1.0.0")
+                                        .addTag("houstonia")
+                                        .build();
+    const doc= SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('/api/docs',app,doc) ;
 
     await app.listen(PORT,()=>{console.log( `PORT is ${PORT}`)})
 }
